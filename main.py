e8c10541bd860ee7559b2fbfd89c06f938c7482d
Freddy B <52885763+ingxfm@users.noreply.github.com>
2022-05-03 14:22:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ controls = {
 }
 
 
-
 print('To play TTT (tic-tac-toe), press a number from 1 to 9.\n'
       'From 1 to 3, for the first line, from left to right.\n'
-      'From 4 to 6, for the second line.\n'
-      'From 7 to 9, for the third line.\n')
+      'From 4 to 6, for the second line, from left to right.\n'
+      'From 7 to 9, for the third line, from left to right.\n')
 
 for key in controls:
     print(f'{controls[key]}')
@@ -18,5 +17,10 @@ print('\n')
 
 play = input('Press a number key from 1 to 9 to select a position: ')
 
-if int(play) in controls:
-    print('yeah')
+value = 0
+for key in controls:
+    if int(play) in controls[key]:
+        controls[key][controls[key].index(int(play))] = 'x'
+        for key in controls:
+            print(f'{controls[key]}')
+        print('yeah')
